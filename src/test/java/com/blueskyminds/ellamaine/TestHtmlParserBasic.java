9f445a956869f8efc0cd898a7110d264402cffb6
jeromyevans <jeromy.evans@blueskyminds.com.au>
2007-02-10 04:46:43 +0000

    Integrated HTMLParser(extractor) into AdvertisementService for testing (import from HTML) Updated address scoring - addresses can now reference only a suburb
    Ellamaine is no longer dependent on wdis - the dependency has been reversed - extractors are now in wdis (temporarily)

---
+++ src/test/java/com/blueskyminds/ellamaine/TestHtmlParserBasic.java
@@ -0,0 +1,48 @@
+package com.blueskyminds.ellamaine;
+
+import com.blueskyminds.framework.test.BaseTestCase;
+import com.blueskyminds.tools.Configuration;
+import com.blueskyminds.ellamaine.html.HtmlParser;
+import com.blueskyminds.ellamaine.html.TextExtractor;
+import java.net.URL;
+import java.io.InputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Date Started: 8/12/2006
+ * <p/>
+ * History:
+ * <p/>
+ * Copyright (c) 2006 Blue Sky Minds Pty Ltd<br/>
+ */
+public class TestHtmlParserBasic extends BaseTestCase {
+
+    private static final Log LOG = LogFactory.getLog(TestHtmlParserBasic.class);
+    public TestHtmlParserBasic(String string) {
+        super(string);
+    }
+
+    // ------------------------------------------------------------------------------------------------------
+
+    /**
+     * Initialise the TestHtmlParserBasic with default attributes
+     */
+    private void init() {
+    }
+
+    // ------------------------------------------------------------------------------------------------------
+
+    public void testHtmlParser() throws Exception {
+        URL fileUrl = Configuration.locateResource("rsearch1.htm");
+        InputStream inputStream = fileUrl.openStream();
+        HtmlParser parser = new HtmlParser();
+        parser.registerExtractor(new TextExtractor());
+        String textContent = (String) parser.parseDocument("rsearch1.htm", inputStream);
+        assertNotNull(textContent);
+        System.out.println(textContent);
+    }
+
+
+}
