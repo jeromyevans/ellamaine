410f439e58e5aba77ce9c88a242befe44b15cdb1
jeromyevans <jeromy.evans@blueskyminds.com.au>
2008-08-21 17:45:05 +0000

    Updated script to migrate existing records

--- ellamaine-repository-web/src/main/sql/updateAdvertisementRepository.sql
+++ ellamaine-repository-web/src/main/sql/updateAdvertisementRepository.sql
@@ -8,7 +8,49 @@ update advertisementRepository
   set Datestamp = date(dateEntered),
       Year = year(dateEntered),
       Month = month (dateEntered),
-      Day = day(dateEntered);
-
-
-
+      Day = day(dateEntered) where id < 10000000;
+update advertisementRepository
+  set Datestamp = date(dateEntered),
+      Year = year(dateEntered),
+      Month = month (dateEntered),
+      Day = day(dateEntered) where id >= 1000000 and id < 2000000;
+update advertisementRepository
+  set Datestamp = date(dateEntered),
+      Year = year(dateEntered),
+      Month = month (dateEntered),
+      Day = day(dateEntered) where id >= 2000000 and id < 3000000;
+update advertisementRepository
+  set Datestamp = date(dateEntered),
+      Year = year(dateEntered),
+      Month = month (dateEntered),
+      Day = day(dateEntered) where id >= 3000000 and id < 4000000;
+update advertisementRepository
+  set Datestamp = date(dateEntered),
+      Year = year(dateEntered),
+      Month = month (dateEntered),
+      Day = day(dateEntered) where id >= 4000000 and id < 5000000;
+update advertisementRepository
+  set Datestamp = date(dateEntered),
+      Year = year(dateEntered),
+      Month = month (dateEntered),
+      Day = day(dateEntered) where id >= 5000000 and id < 6000000;
+update advertisementRepository
+  set Datestamp = date(dateEntered),
+      Year = year(dateEntered),
+      Month = month (dateEntered),
+      Day = day(dateEntered) where id >= 6000000 and id < 7000000;
+update advertisementRepository
+  set Datestamp = date(dateEntered),
+      Year = year(dateEntered),
+      Month = month (dateEntered),
+      Day = day(dateEntered) where id >= 7000000 and id < 8000000;
+update advertisementRepository
+  set Datestamp = date(dateEntered),
+      Year = year(dateEntered),
+      Month = month (dateEntered),
+      Day = day(dateEntered) where id >= 8000000 and id < 9000000;
+update advertisementRepository
+  set Datestamp = date(dateEntered),
+      Year = year(dateEntered),
+      Month = month (dateEntered),
+      Day = day(dateEntered) where id >= 9000000 and id < 10000000;
